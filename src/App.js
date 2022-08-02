305d24e4fba66da2427c7b5d62cb452fc42cc2d4
dev <dev@grupocci.local>
2022-08-02 16:41:52 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>Turma de programação FullStaks</code> Escola CCi
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Alo mundo! CCI
         </a>
       </header>
     </div>
